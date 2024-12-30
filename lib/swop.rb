7d178a73e559fb7c8738ee82e7e3028135ec3b45
Kamz16 <kamilw175@gmail.com>
2024-12-30 17:53:58 +0000

    Build gem

---
+++ lib/swop.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "swop/version"
+
+module Swop
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/swop/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Swop
+  VERSION = "0.1.0"
+end
